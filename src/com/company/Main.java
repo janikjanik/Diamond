ef72c641f6e7ec2147541e0f9da7e4d4ccffdd99
Michal Janik <michal.k.janik@gmail.com>
2018-06-14 10:48:31 +0200

    added method for printing single letter

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+//        char c = (char) System.in.read();
+//        c = Character.toUpperCase(c);
+//        Character a = 'A';
+//        while((int)a != (int)c + 1){
+//            while()
+//            a = (char) ((int)a + 1);
+//        }
+    }
+
+    public void printSingleLetter(int spaces, char letter){
+        int i = 0;
+        while (i < spaces){
+            System.out.println(" ");
+        }
+        System.out.println(letter);
     }
 }
