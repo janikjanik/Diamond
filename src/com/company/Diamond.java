4947ded90153a1dfd16a32909b82c630bdceac6c
Michal Janik <michal.k.janik@gmail.com>
2018-06-14 15:06:06 +0200

    Improved version od Diamond

---
+++ src/com/company/Diamond.java
@@ -0,0 +1,67 @@
+package com.company;
+
+import java.io.IOException;
+
+public class Diamond {
+    private char letterFromInput;
+    private int firstSpace;
+    private char currentLetter = 'A';
+    private int secondSpace = 1;
+
+    public Diamond(char letterFromInput) {
+        this.letterFromInput = Character.toUpperCase(letterFromInput);
+        this.firstSpace = letterFromInput - currentLetter;
+    }
+
+    public static void main(String[] args) throws IOException {
+         char inputLetter = (char) System.in.read();
+         Diamond diamond = new Diamond(inputLetter);
+         diamond.makeDiamond();
+    }
+
+    public void makeDiamond(){
+        if(letterFromInput == 'A') {
+            System.out.println("A");
+        }
+        else {
+            printSingleLetter(firstSpace, currentLetter);
+            firstSpace -= 1;
+            System.out.println();
+            printFirstHalfOfDiamond();
+            firstSpace += 2;
+            secondSpace -= 4;
+            printSecondHalfOfDiamond();
+            printSingleLetter(firstSpace, 'A');
+        }
+    }
+
+    public void printFirstHalfOfDiamond(){
+        while (currentLetter != letterFromInput) {
+            currentLetter += 1;
+            printSingleLetter(firstSpace, currentLetter);
+            printSingleLetter(secondSpace, currentLetter);
+            System.out.println();
+            secondSpace += 2;
+            firstSpace -= 1;
+        }
+    }
+
+    public void printSecondHalfOfDiamond(){
+        while (currentLetter != 'B') {
+            currentLetter -= 1;
+            printSingleLetter(firstSpace, currentLetter);
+            printSingleLetter(secondSpace, currentLetter);
+            System.out.println();
+            secondSpace -= 2;
+            firstSpace += 1;
+        }
+    }
+
+    public static void printSingleLetter(int spaces, char letter){
+        int i = 0;
+        while (i++ < spaces){
+            System.out.print(" ");
+        }
+        System.out.print(letter);
+    }
+}
